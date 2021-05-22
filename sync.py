ae6a0563efb7c36add9b9ac93045e2c57f3609b5
Mark Essien <markessien@gmail.com>
2021-05-22 13:32:22 +0200

    Added a sync script

---
+++ sync.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+import subprocess
+
+
+settings_file = "sync.yaml"
+
+with open(settings_file, 'r') as stream:
+    print("Loaded settings file: " + settings_file)
+    sync_settings = yaml.safe_load(stream)
+
+for sync_name, sync in sync_settings.items():
+    src = os.path.join(sync["Src"], "*.plot")
+    dst = sync["Dst"]
+    host = sync["Host"]
+    username = sync["Username"]
+
+    command = "rsync -av --progress " + src + " " + username + "@" + host + ":" + dst
+    print("Running: " + command)
+    subprocess.call(command, shell=True)
