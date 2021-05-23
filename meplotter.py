5a8c0e8c4ed4ed034abd8b73c0b01682bbd1c440
Mark Essien <markessien@gmail.com>
2021-05-23 14:17:50 +0200

    Added the ability to specify multiple plotters and open in new tabs

--- meplotter.py
+++ meplotter.py
@@ -17,15 +17,15 @@ python3 meplotter.py Plotter1 --cleanup 1 --delay 30m
 
 '''
 
-
 parser = argparse.ArgumentParser(description='Manage your Plotting')
-parser.add_argument('name', help='The name of the plotter as specified in the settings')
+parser.add_argument('plotters', nargs='*', help='The name(s) of the plotter(s) as specified in the settings')
 
 parser.add_argument('--delay', help='Time to delay before starting (e.g 10m, 3h)')
 parser.add_argument('--cleanup', help='Delete all tmp files in the temporary directory')
 parser.add_argument("--settings", help="Specify the settings file")
 
 args = parser.parse_args()
+
 
 chia_folder = os.path.join(Path.home(), "chia-blockchain/")
 k_type = "32"
@@ -36,67 +36,87 @@ settings_file = "plotters.yaml"
 
 print("Welcome to the MePlotter")
 
+print("Starting " + str(args.plotters))
+
 if args.settings:
     settings_file = args.settings
 
-# Read settings file
-with open(settings_file, 'r') as stream:
-    print("Loaded settings file: " + settings_file)
-    plotting_settings = yaml.safe_load(stream)
+commands = []
 
-    plotter_settings = plotting_settings[args.name]
+for i, plotter in enumerate(args.plotters):
 
-    tmp_folder = plotter_settings['Tmp']
-    dst_folder = plotter_settings['Dst']
+    # Read settings file
+    with open(settings_file, 'r') as stream:
+        print("Loaded settings file: " + settings_file)
+        plotting_settings = yaml.safe_load(stream)
 
-    if 'Type' in plotter_settings:
-        k_type = plotter_settings['Type']
+        plotter_settings = plotting_settings[plotter]
 
-    if 'Queue' in plotter_settings:
-        queue_length = plotter_settings['Queue']
+        tmp_folder = plotter_settings['Tmp']
+        dst_folder = plotter_settings['Dst']
 
-    if 'Memory' in plotter_settings:
-        memory_size = plotter_settings['Memory']
+        if 'Type' in plotter_settings:
+            k_type = plotter_settings['Type']
 
-    if 'Threads' in plotter_settings:
-        number_threads = plotter_settings['Threads']
+        if 'Queue' in plotter_settings:
+            queue_length = plotter_settings['Queue']
 
-    print("Executing " + args.name)
+        if 'Memory' in plotter_settings:
+            memory_size = plotter_settings['Memory']
 
-if not tmp_folder or not dst_folder:
-    print("Your destination or temp folders are not set")
-    exit()
+        if 'Threads' in plotter_settings:
+            number_threads = plotter_settings['Threads']
 
-if args.cleanup:
-    print("Cleaning up the folders")
-    files = glob.glob(os.path.join(tmp_folder, "*.tmp"))
-    for f in files:
-        os.remove(f)
+        print("Executing " + plotter)
 
-try:
-    os.makedirs(tmp_folder)
-except:
-    pass
+    if not tmp_folder or not dst_folder:
+        print("Your destination or temp folders are not set")
+        exit()
 
-try:
-    os.makedirs(dst_folder)
-except:
-    pass
+    if args.cleanup:
+        print("Cleaning up the folders")
+        files = glob.glob(os.path.join(tmp_folder, "*.tmp"))
+        for f in files:
+            os.remove(f)
 
-if args.delay:
-    delay = Duration(args.delay)
-    print("Waiting for " + str(delay))
-    time.sleep(delay.to_seconds())
+    try:
+        os.makedirs(tmp_folder)
+    except:
+        pass
 
-command = ". " + os.path.join(chia_folder, 'activate') + " && " + \
-            os.path.join(chia_folder, 'venv/bin/chia') + \
-            " plots create" + \
-            " -k " + str(k_type) + \
-            " -n " + str(queue_length) + \
-            " -t " + tmp_folder + \
-            " -d " + dst_folder + \
-            " -b " + str(memory_size) + \
-            " -u 128" + \
-            " -r " + str(number_threads)
+    try:
+        os.makedirs(dst_folder)
+    except:
+        pass
 
-subprocess.run([command], shell=True, stdout=sys.stdout)
+    sleep_cmd = ""
+    if args.delay:
+        
+        delay = Duration(args.delay)
+        delay_s = delay.to_seconds()
+        if len(args.plotters) > 1:
+            delay_s += delay_s*(i-1)
+
+        print("Waiting for " + str(delay))
+        sleep_cmd = "echo MePlotter. " + plotter + " waiting for " + str(delay_s) + "s... && sleep " + str(delay_s) + " && "
+
+    
+    command = sleep_cmd + ". " + os.path.join(chia_folder, 'activate') + " && " + \
+                os.path.join(chia_folder, 'venv/bin/chia') + \
+                " plots create" + \
+                " -k " + str(k_type) + \
+                " -n " + str(queue_length) + \
+                " -t " + tmp_folder + \
+                " -d " + dst_folder + \
+                " -b " + str(memory_size) + \
+                " -u 128" + \
+                " -r " + str(number_threads)
+
+    commands.append(command)
+
+terminal_cmd = "gnome-terminal"
+for cmd in commands:
+    terminal_cmd = terminal_cmd + " --tab -e 'bash -c \"" + cmd + "; read -n1\"'"
+print(terminal_cmd)
+
+subprocess.run([terminal_cmd], shell=True, stdout=sys.stdout)
