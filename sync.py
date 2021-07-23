e5dad6d584f2ab44bfbd69f25a36b44e6522d7d9
Mark Essien <markessien@gmail.com>
2021-07-23 12:10:01 +0200

    Some fixes for local and network transfer

--- sync.py
+++ sync.py
@@ -1,9 +1,10 @@
 import os
 import yaml
 import time
+import shutil
 import subprocess
 
-
+SPACE_FREE_REQUIRED = 322122547200
 settings_file = "sync.yaml"
 
 while True:
@@ -20,10 +21,25 @@ while True:
     for sync_name, sync in sync_settings.items():
         src = os.path.join(sync["Src"], "*.plot")
         dst = sync["Dst"]
-        host = sync["Host"]
-        username = sync["Username"]
 
-        command = "rsync -av --progress --remove-source-files " + src + " " + dst
+        try:
+            os.makedirs(dst)
+        except:
+            pass
+
+        if "Host" in sync:
+            host = sync["Host"]
+            username = sync["Username"]
+            command = "rsync -av --progress --remove-source-files " + src + " " + username + "@" + host + ":" + dst
+        else:
+            stat = shutil.disk_usage(dst)
+            print("Disk Free " + str(stat.free))
+
+            if stat.free < SPACE_FREE_REQUIRED:
+                print("Not enough space, skipping")
+            else:
+                command = "rsync -av --progress --remove-source-files " + src + " " + dst
+
         print("Running: " + command)
         subprocess.call(command, shell=True)
     
